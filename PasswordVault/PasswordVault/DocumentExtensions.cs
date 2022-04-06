783d5e70bb7c80cd1858dbd558147c6b1df80725
smitbre2 <brendenmsmith@hotmail.com>
2022-04-05 20:48:58 -0700

    Created easy document conversion for XML and Linq

---
+++ PasswordVault/PasswordVault/DocumentExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace PasswordVault
+{
+    // Using var blocks will call dispose on exit making cleanup easier
+    public static class DocumentExtensions
+    {
+        public static XmlDocument ToXmlDocument(XDocument xDoc)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            using (var xmlReader = xDoc.CreateReader())
+            {
+                xmlDoc.Load(xmlReader);
+            }
+            return xmlDoc;
+        }
+
+        public static XDocument ToXDocument(XmlDocument xmlDoc)
+        {
+            using (var nodeReader = new XmlNodeReader(xmlDoc))
+            {
+                nodeReader.MoveToContent();
+                return XDocument.Load(nodeReader);
+            }
+        }
+    }
+}
